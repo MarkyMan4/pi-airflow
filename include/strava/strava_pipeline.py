32538248ce09df61827355063b7766f7973043fe
MarkyMan4 <markus.hutnik@gmail.com>
2024-09-13 19:43:49 -0500

    removed commented out code

--- include/strava/strava_pipeline.py
+++ include/strava/strava_pipeline.py
@@ -37,7 +37,6 @@ def strava_source() -> Any:
         },
         # The default configuration for all resources and their endpoints
         "resource_defaults": {
-            # "primary_key": "id",
             "write_disposition": "merge",
             "endpoint": {
                 "params": {
@@ -56,20 +55,9 @@ def strava_source() -> Any:
                 "name": "activities",
                 "endpoint": {
                     "path": "/athlete/activities",
-                    # Query parameters for the endpoint
                     # api docs https://developers.strava.com/docs/reference/#api-Activities-getLoggedInAthleteActivities
                     "params": {
                         "after": 1725160601
-                        # Define `since` as a special parameter
-                        # to incrementally load data from the API.
-                        # This works by getting the updated_at value
-                        # from the previous response data and using this value
-                        # for the `since` query parameter in the next request.
-                        # "since": {
-                        #     "type": "incremental",
-                        #     "cursor_path": "updated_at",
-                        #     "initial_value": "2024-01-25T11:21:28Z",
-                        # },
                     },
                 },
             },
