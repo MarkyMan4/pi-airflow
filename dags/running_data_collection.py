a8ffd808651aa73ab330ba0a95fc282db3c526c0
MarkyMan4 <markus.hutnik@gmail.com>
2024-09-17 23:24:26 -0500

    added dag description

--- dags/running_data_collection.py
+++ dags/running_data_collection.py
@@ -5,6 +5,7 @@ from airflow.decorators import dag, task
 
 
 @dag(
+    description="Collect running data from strava and store in DuckDB",
     start_date=pendulum.datetime(2024, 9, 8, tz="America/Chicago"),
     schedule="0 */4 * * *",
     catchup=False
@@ -12,8 +13,8 @@ from airflow.decorators import dag, task
 def running_data_collection():
     @task
     def strava_to_duckdb():
-        import sys
         import os
+        import sys
         sys.path.append(os.path.abspath(os.environ["AIRFLOW_HOME"]))
 
         from include.strava.strava_pipeline import load_strava
